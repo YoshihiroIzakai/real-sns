7e1bfd8c275fa3ee885077cbd17f1758856d37a0
Yoshihiro <cguj1024@mail4.doshisha.ac.jp>
2025-05-21 15:39:45 +0900

    コミットメッセージ

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,6 @@ import "./Rightbar.css"
 import { Users } from "../../dummyData"
 import Online from '../online/Online'
 
-
 export default function Rightbar({ user }) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 
@@ -11,12 +10,12 @@ export default function Rightbar({ user }) {
     return(
       <>
         <div className="eventContainer">
-          <img src="./assets/star.png" alt="" className="starImg"/>
+          <img src={PUBLIC_FOLDER + "star.png"} alt="" className="starImg"/>
           <span className="eventText">
             <b>フォロワー限定</b>イベント開催中！
           </span>
         </div>
-        <img src="assets/event.jpeg" alt="" className="eventImg"/>
+        <img src={PUBLIC_FOLDER +"event.jpeg"} alt="" className="eventImg"/>
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           {Users.map((user) => (
@@ -25,19 +24,19 @@ export default function Rightbar({ user }) {
         </ul>
         <p className="promotionTitle">プロモーション広告</p>
         <img 
-          src="assets/promotion/promotion1.jpeg" 
+          src={PUBLIC_FOLDER + "promotion/promotion1.jpeg"}
           alt=""  
           className="rightbarPromotionImg"
         />
         <p className="promotionName">ショッピング</p>
         <img 
-          src="assets/promotion/promotion2.jpeg" 
+          src={PUBLIC_FOLDER + "promotion/promotion2.jpeg"} 
           alt=""  
           className="rightbarPromotionImg"
         />
         <p className="promotionName">カーショップ</p>
         <img 
-          src="assets/promotion/promotion3.jpeg" 
+          src={PUBLIC_FOLDER + "promotion/promotion3.jpeg"}
           alt=""  
           className="rightbarPromotionImg"
         />
